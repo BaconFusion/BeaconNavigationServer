f2571b835b833395c458d3d55699c9be959d1db7
glor <glor@gitlab.com>
2016-09-15 10:34:57 +0200

    move send/receive to Beacon class

--- Main/src/me/glor/BeaconNavigationServer/Server.java
+++ Main/src/me/glor/BeaconNavigationServer/Server.java
@@ -3,7 +3,7 @@ package me.glor.BeaconNavigationServer;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by glor on 9/14/16.
@@ -26,41 +26,10 @@ public class Server implements Runnable {
 			e.printStackTrace();
 			return;
 		}
-
-		int beaconCount = 0;    // 2 bytes
 		try {
-			// read in as unsigned
-			beaconCount |= dis.readShort();
+			Collection<Beacon> beacons = Beacon.readBeacons(dis);
 		} catch (IOException e) {
-			System.err.println("Error while reading data.");
 			e.printStackTrace();
-			return;
 		}
-		ArrayList<Beacon> beacons = new ArrayList<Beacon>(beaconCount);
-
-		long uuid1, uuid2;    // 8+8 = 16 byte
-		short major, minor;    // 2 byte each
-		short rssi = 0;
-		beaconCount = 1;
-		for (int i = 0; i < beaconCount; i++) {
-			try {
-				uuid1 = dis.readLong();
-				uuid2 = dis.readLong();
-				major = dis.readShort();
-				minor = dis.readShort();
-				// read in as unsigned
-				rssi |= dis.readByte();
-			} catch (IOException e) {
-				System.err.println("Error while reading data.");
-				e.printStackTrace();
-				return;
-			}
-			beacons.add(new Beacon(uuid1, uuid2, major, minor, rssi));
-		}
-		for (Beacon beacon : beacons) {
-			System.out.println("Received: " + beacon);
-		}
-
-
 	}
 }
